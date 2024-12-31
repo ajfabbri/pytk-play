21750049524cfc10a5be8c2a264a9d7c729d9ee3
Aaron Fabbri <558234+ajfabbri@users.noreply.github.com>
2024-12-30 20:25:19 -0800

    Hacking on pomodoro app

--- main.py
+++ main.py
@@ -1,41 +1,89 @@
 #!/usr/bin/env python
 import tkinter as tk
+from tkinter import ttk
 import threading
 import time
 
-class Application(tk.Frame):
+import sound
+from sound import Sound
+
+class Application(ttk.Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
+        super().__init__(master)
         self.createWidgets()
 
         # timer state
         self.started_timer = False
+        self.pack()
 
-    # 2x2 grid
+        # sound engine
+        self.sound = sound.Sound()
+        self.sound.play(sound.Samples.STARTUP)
+
     def createWidgets(self):
-        self.armButton = tk.Button(self, text='Start',
+
+        style = ttk.Style(self)
+        # dark mode settings
+        style.theme_use('clam')
+        style.configure("TButton", padding=6, relief="flat", background="black", foreground="white")
+        style.configure("TLabel", padding=6, relief="flat", background="black", foreground="white")
+        style.configure("TFrame", padding=6, relief="flat", background="black", foreground="white")
+        style.configure("TLabel", padding=6, relief="flat", background="black", foreground="white")
+
+        # style for large-text clock label
+        style.configure("TLabel.Large", padding=6, relief="flat", background="black", foreground="white", font=("Helvetica", 44, "bold"))
+        style.layout("TLabel.Large", [('Label.border', {'sticky': 'nswe',
+            'border': '1', 'children': [('Label.padding', {'sticky': 'nswe',
+            'children': [('Label.label', {'sticky': 'nswe'})]})]})])
+
+        #filename = "img\\tomato-pixel.jpg"
+        filename = "img\\tomato-pixel.png"
+        img = tk.PhotoImage(width=400, height=400, file=filename)
+        self.logo = ttk.Label(self, image=img)
+        self.logo.pack()
+
+        self.armButton = ttk.Button(self, text='Start',
             command=self.arm)
-        self.armButton.grid(row=0, column=0, padx=10, pady=10)
+        self.armButton.pack()
 
-        self.timeLabel = tk.Label(self, text="OO:OO")
-        self.timeLabel.grid(row=0, column=1)
+        self.timeLabel = ttk.Label(self, text="OO:OO", style="TLabel.Large")
+        self.timeLabel.pack()
 
-        self.quitButton = tk.Button(self, text='Quit',
+        self.quitButton = ttk.Button(self, text='Quit',
             command=self.quit)
-        self.quitButton.grid(column=1, row=1)
+        self.quitButton.pack()
 
+    # for Timer to update the time display
     def setTimeText(self, text: str):
         self.timeLabel.config(text=text)
 
-    def arm(self, secs=120):
+    # for Timer to notify us when expired
+    def onTimerExpired(self):
+        self.sound.play(sound.Samples.TIMER_EXPIRED2)
+        time.sleep(1.2)
+        self.sound.play(sound.Samples.TIMER_EXPIRED1)
+        self.started_timer = False
+
+    def arm(self, secs=8):
         if self.started_timer:
             print("ignoring arm butt: timer already started")
+            self.sound.play(sound.Samples.TIMER_STOP)
             return
 
         print(f"Starting {secs} second timer")
         self.timer = MyTimer(self)
         self.timer.start(secs)
+        self.started_timer = True
+
+    def quit(self):
+        self.sound.play(sound.Samples.SHUTDOWN)
+        time.sleep(0.3)
+        super().quit()
+
+    def destroy(self):
+        self.sound.play(sound.Samples.ABRUPT_SHUTDOWN)
+        time.sleep(0.5)
+        super().destroy()
 
 class MyTimer(threading.Thread):
     def __init__(self, app: Application):
@@ -64,13 +112,24 @@ class MyTimer(threading.Thread):
             secs_left = self.duration - elapsed
             if secs_left <= 0:
                 self.app.setTimeText("00:00")
+                self.app.onTimerExpired()
                 print("Timer expired, exiting timer loop")
                 break
             whole_mins = int(secs_left) / 60
             mins = float(whole_mins)
             secs = int(secs_left) % 60
-            self.app.setTimeText(f"{mins:2.0f}:{secs:2.0f}")
+            self.app.setTimeText(f"{mins:02.0f}:{secs:02.0f}")
 
+# main
 app = Application()
-app.master.title('Sample application')
+app.master.title('Pomodoro Timer')
+app.master.maxsize(400, 400)
+
+# handle shudwoen to play exit sound
+def on_closing():
+    app.destroy()
+    tk.Tk().quit()
+
+app.master.protocol("WM_DELETE_WINDOW", on_closing)
+
 app.mainloop()
---
+++ sound.py
@@ -0,0 +1,27 @@
+import enum
+import pygame
+
+class Samples(enum.StrEnum):
+    STARTUP = "woosh-motion.mp3"
+    TIMER_START = "game-start.mp3"
+    TIMER_EXPIRED1 = "level-up.mp3"
+    TIMER_EXPIRED2 = "item-pick-up.mp3"
+    TIMER_TICK = "tick.mp3"
+    TIMER_STOP = "down-fart.mp3"
+    SHUTDOWN = "gameboy-pluck.mp3"
+    ABRUPT_SHUTDOWN = "kid-bye.mp3"
+
+
+# uses pygame mixer to play sound samples as requested
+class Sound:
+    def __init__(self):
+        self.sound = None
+
+    def play(self, sample: Samples):
+        if self.sound:
+            self.sound.stop()
+        # init mixer if not already
+        if not pygame.mixer.get_init():
+            pygame.mixer.init()
+        self.sound = pygame.mixer.Sound("sound/" + sample.value)
+        self.sound.play()
